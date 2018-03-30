e16d3ced57b8a901fb46f7fb99e441fa0a698f26
modest-as <modestas.petravicius@gmail.com>
2018-03-30 01:57:46 +0100

    initial commit

---
+++ uninstall-windows-crap.ps1
@@ -0,0 +1,7 @@
+# Remove all crap axcept calculator. -AllUsers optional
+
+Get-AppXPackage | where-object {$_.name –notlike “*calculator*”} | Remove-AppxPackage
+
+# DIE CORTANA
+New-Item -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\' -Name 'Windows Search' | Out-Null
+New-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search' -Name 'AllowCortana' -PropertyType DWORD -Value '0' | Out-Null
