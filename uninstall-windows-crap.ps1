573563f9a86c6f553802cc3033c24a3c6e90f744
Modest-as <modestas.petravicius@gmail.com>
2021-02-25 17:42:10 +0200

    update

--- uninstall-windows-crap.ps1
+++ uninstall-windows-crap.ps1
@@ -1,6 +1,6 @@
 # Remove all crap axcept calculator. -AllUsers optional
 
-Get-AppXPackage | where-object {$_.name –notlike “*calculator*”} | Remove-AppxPackage
+Get-AppXPackage | where-object {$_.name -notlike "*calculator*" -and $_.name -notlike "*photo*" -and $_.name -notlike "*store*"} | Remove-AppxPackage
 
 # DIE CORTANA
 New-Item -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\' -Name 'Windows Search' | Out-Null
